039c0116bafd6ce79d6cc2bfacac91cbfb2169ce
Gilbert <35531820+olayenilawal@users.noreply.github.com>
2024-05-13 17:17:15 +0100

    dxd

--- js/home.js
+++ js/home.js
@@ -82,20 +82,3 @@ window.onload = function() {
     getTotalMaleStaff();
     getTotalITStaff();
 };
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    const dropdownArrows = document.querySelectorAll(".dropdown-arrow");
-  
-    dropdownArrows.forEach((arrow) => {
-      arrow.addEventListener("click", function () {
-        const dropdown = this.parentNode.querySelector("select");
-        if (dropdown.style.display === "none" || dropdown.style.display === "") {
-          dropdown.style.display = "block";
-        } else {
-          dropdown.style.display = "none";
-        }
-      });
-    });
-  });
-  
